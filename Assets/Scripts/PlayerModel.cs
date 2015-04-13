eec1454c600535d0e220192140ea1ff87216ec46
John Metcalfe <jmetcalfe201@gmail.com>
2015-04-13 17:49:14 -0500

    Fixed Player movement and added a spike trap to test player health

--- Assets/Scripts/PlayerModel.cs
+++ Assets/Scripts/PlayerModel.cs
@@ -7,7 +7,7 @@ public class PlayerModel : MonoBehaviour
 	[SerializeField] PlayerController controller;
 
 	[SerializeField] protected float jumpForce;
-	[SerializeField] protected float moveForce;
+	[SerializeField] protected float moveVelocity;
 	[SerializeField] protected int health;
 
 	protected Rigidbody2D physics;
@@ -22,14 +22,47 @@ public class PlayerModel : MonoBehaviour
 	
 	public void MoveRight(float amount)
 	{
-		// Adds a lateral force to the player 
-		//  -amount decides direction (and is a scalar if using a controller)
-		//  -Time.deltaTime makes it so the force is independent of framerate
-		physics.AddForce(new Vector2(moveForce * amount * Time.deltaTime, 0), ForceMode2D.Impulse);
-	}
+		Vector2 wantedVelocity = new Vector2(moveVelocity * amount, physics.velocity.y);
+
+		//want right
+		if(amount > 0)
+		{
+			if(physics.velocity.x > 0)//is moving right
+			{
+				if(wantedVelocity.x > physics.velocity.x)//if wanted speed is more than current
+				{
+					physics.velocity = wantedVelocity; //set the player's speed
+				}else if(wantedVelocity.x < physics.velocity.x)//if wanted speed is less than current
+				{
+					//dont set the speed to lower than current unless switching directions (this keeps inertia if the player keeps the stick neutral)
+				}
+			}else if(physics.velocity.x <= 0)//is moving left
+			{
+				physics.velocity = wantedVelocity; //set the player's speed (switch directions)
+			}
+		}else if(amount < 0) //want left
+		{
+			if(physics.velocity.x < 0)//is moving left
+			{
+				if(wantedVelocity.x < physics.velocity.x)//if wanted speed is more than current
+				{
+					physics.velocity = wantedVelocity; //set the player's speed
+				}else if(wantedVelocity.x > physics.velocity.x)//if wanted speed is less than current
+				{
+					//dont set the speed to lower than current unless switching directions (this keeps inertia if the player keeps the stick neutral)
+                }
+            }else if(physics.velocity.x >= 0)//is moving right
+            {
+                physics.velocity = wantedVelocity; //set the player's speed (switch directions)
+            }
+        }
+    }
 	
 	public void Jump()
 	{
+		// Zero out the player's vertical speed to combat compounded jumping
+		physics.velocity = new Vector2(physics.velocity.x, 0);
+		
 		// Adds an upward force to the player
 		physics.AddForce(new Vector2(0 , jumpForce), ForceMode2D.Impulse);
 	}
@@ -37,10 +70,18 @@ public class PlayerModel : MonoBehaviour
 	public void TakeDamage(int amount)
 	{
 		health -= amount;
+		
+		// Pass the update up the chain
 		controller.OnUpdateHealth(health);
+		
+		if(health <= 0)
+		{
+			health = 0;
+			Die ();
+		}
 	}
 	
-	public void Die()
+	protected void Die()
 	{
 		// Temp
 		Debug.Log("Player is dead");
---
+++ Assets/Scripts/SpikeTrap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeTrap : MonoBehaviour
+{
+	[SerializeField] protected int damage;
+	protected bool alreadyDoneDamage = false;
+	
+	protected void OnTriggerEnter2D(Collider2D col)
+	{
+		Debug.Log("trigger overlap");
+	
+		if(col.name.Equals("Player") && !alreadyDoneDamage)
+		{
+			col.gameObject.GetComponent<PlayerModel>().TakeDamage(damage);
+			alreadyDoneDamage = true;
+		}
+	}
+	
+	protected void OnTriggerExit2D(Collider2D col)
+	{
+		if(col.name.Equals("Player"))
+		{
+			alreadyDoneDamage = false;
+		}
+	}
+}
