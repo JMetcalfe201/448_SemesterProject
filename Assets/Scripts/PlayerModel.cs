0274f2d9482f16f76704336b10094d545101f1bf
John Metcalfe <jmetcalfe201@gmail.com>
2015-04-14 12:54:43 -0500

    Limited amount of player jumps and added TimerManager.cs

--- Assets/Scripts/PlayerModel.cs
+++ Assets/Scripts/PlayerModel.cs
@@ -6,8 +6,13 @@ public class PlayerModel : MonoBehaviour
 {
 	[SerializeField] PlayerController controller;
 
-	[SerializeField] protected float jumpForce;
 	[SerializeField] protected float moveVelocity;
+	
+	[SerializeField] protected float jumpForce;
+	protected bool isGrounded;
+	protected int numJumps;
+	[SerializeField] protected int maxJumps;
+	
 	[SerializeField] protected int health;
 
 	protected Rigidbody2D physics;
@@ -15,9 +20,27 @@ public class PlayerModel : MonoBehaviour
 	// Use this for initialization (inherated from MonoBehavior)
 	protected void Start () 
 	{
+		isGrounded = false;
+		numJumps = 0;
+		
+		// Make sure the player can jump at least once
+		if(maxJumps < 1)
+		{
+			maxJumps = 1;
+		}
+		
 		physics = GetComponent<Rigidbody2D>();
 		
 		controller.OnUpdateHealth(health);
+	}
+	
+	protected void Update ()
+	{
+		// If not on the ground or falling (don't check when rising or player could get extra jump)
+		if(!isGrounded && physics.velocity.y <= 0)
+		{
+			CheckGrounding();
+		}
 	}
 	
 	public void MoveRight(float amount)
@@ -60,11 +83,17 @@ public class PlayerModel : MonoBehaviour
 	
 	public void Jump()
 	{
-		// Zero out the player's vertical speed to combat compounded jumping
-		physics.velocity = new Vector2(physics.velocity.x, 0);
-		
-		// Adds an upward force to the player
-		physics.AddForce(new Vector2(0 , jumpForce), ForceMode2D.Impulse);
+		if(numJumps < maxJumps)
+		{
+			// Zero out the player's vertical speed to combat compounded jumping
+			physics.velocity = new Vector2(physics.velocity.x, 0);
+			
+			// Adds an upward force to the player
+			physics.AddForce(new Vector2(0 , jumpForce), ForceMode2D.Impulse);
+			
+			isGrounded = false;
+			numJumps++;
+		}
 	}
 	
 	public void TakeDamage(int amount)
@@ -93,5 +122,34 @@ public class PlayerModel : MonoBehaviour
 		physics.fixedAngle = false;
 		physics.AddForce(new Vector2(Random.Range(-3,3), 25), ForceMode2D.Impulse);
 		physics.AddTorque(500f);
+	}
+	
+	public void CheckGrounding()
+	{
+		BoxCollider2D col = GetComponent<BoxCollider2D>();
+	
+		RaycastHit2D hit;
+		
+		// Slide the player collider down 0.05 units to check if there is an object on the Ground layer to see if there is ground below
+		hit = Physics2D.BoxCast(gameObject.transform.position + new Vector3(col.offset.x, col.offset.y, 0), col.size, 0, new Vector2(0, -1), 0.05f, 1 << 8);
+		
+		// Check if anythign was hit
+		if(hit.collider)
+		{
+			Debug.Log("Ground Hit");
+			isGrounded = true;
+			numJumps = 0;
+		}
+		
+		Debug.Log("Check grounding: " + isGrounded);
+    }
+	
+	protected void OnCollisionExit2D(Collision2D collision)
+	{
+		if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+		{
+			Debug.Log("Left ground");
+			TimerManager.GetWorldTimerManager().AddTimer(0.1f, this.CheckGrounding);
+		}
 	}
 }
---
+++ Assets/Scripts/TimerManager.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TimerManager : MonoBehaviour 
+{
+	public delegate void Delegate();
+
+	protected struct Timer
+	{
+		public float expireTime;
+		public Delegate expireFunction;
+		public bool repeat;
+		public float length;
+		public int repeatedTimes;
+		public int maxRepeatTimes;
+	}
+	
+	protected List<Timer> timers;
+
+	// Use this for initialization
+	protected void Start () 
+	{
+		// Name the object so it can be retreived later
+		gameObject.name = "World Timer Manager";
+		
+		// Initialize the list of timers
+		timers = new List<Timer>();
+	}
+	
+	// Update is called once per frame
+	protected void Update () 
+	{
+		// Go through all the current timers
+		for(int i = 0; i < timers.Count; i++)
+		{
+			// If the timer is expired
+			if(timers[i].expireTime >= Time.time)
+			{
+				// Call the delegate
+				timers[i].expireFunction();
+				
+				// If the timer should repeat
+				if(timers[i].repeat && (timers[i].maxRepeatTimes == 0 || timers[i].repeatedTimes < timers[i].maxRepeatTimes))
+				{
+					// Recalculate the expire time and increment times repeated
+					float leftOver = Time.time - timers[i].expireTime;
+					Timer timer = timers[i];
+					timer.expireTime += leftOver + timers[i].length;
+					timer.repeatedTimes++;
+					timers[i] = timer;
+					
+					// If this was the last time the timer should expire then remove it instead of waiting to for it to expire again
+					if(timers[i].repeatedTimes == timers[i].maxRepeatTimes)
+					{
+						timers.RemoveAt(i);
+					}
+				}else
+				{
+					// Remove the timer since it is done repeating or shouldn't repeat
+					timers.RemoveAt(i);	
+				}
+			}
+		}
+	}
+	
+	/* Parameters
+	 * - length: the amount of time the timer will wait before calling the expireFunction
+	 * - expireFunction: the function delegate that will be called when the timer expires
+	 * - delay: the amount of time to wait before starting the timer (only effects first run)
+	 * - repeat: whether the timer should repeat
+	 * - maxRepeatTimes: the amount of times the timer should repeat (0 = repeat forever)
+	 */
+	public void AddTimer(float length, Delegate expireFunction, float delay = 0, bool repeat = false, int maxRepeatTimes = 0)
+	{
+		Timer newTimer;
+		
+		newTimer.expireTime = Time.time + delay + length;
+		newTimer.expireFunction = expireFunction;
+		newTimer.repeat = repeat;
+		newTimer.length = length;
+		newTimer.repeatedTimes = 0;
+		newTimer.maxRepeatTimes = maxRepeatTimes;
+		
+		timers.Add(newTimer);
+	}
+	
+	public void RemoveTimer(Delegate expireFunction)
+	{
+		for(int i = 0; i < timers.Count; i++)
+		{
+			if(timers[i].expireFunction == expireFunction)
+			{
+				timers.RemoveAt(i);
+			}
+		}
+	}
+	
+	public static TimerManager GetWorldTimerManager()
+	{
+		return GameObject.Find("World Timer Manager").GetComponent<TimerManager>();
+	}
+}
